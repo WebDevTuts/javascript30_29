966990e739fa719d0fa537ac6735c5aaab01b75b
Edwin Chen <edwinchen85@gmail.com>
2017-05-26 17:08:22 +0800

    Use setInterval to start countdown

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
-  console.log({now, then});   // run test in console, timer(10)
+
+  setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
+    console.log(secondsLeft);
+  }, 1000);
 }

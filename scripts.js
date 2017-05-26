d5536b329aa33fab5e568dab4a566ab837f6e5ca
Edwin Chen <edwinchen85@gmail.com>
2017-05-26 17:49:33 +0800

    Start timer with time set by option button clicked

--- scripts.js
+++ scripts.js
@@ -38,7 +38,7 @@ function displayEndTime(timestamp) {
 
 function startTimer() {
   const seconds = parseInt(this.dataset.time);
-  console.log(seconds);
+  timer(seconds);
 }
 
 buttons.forEach(button => button.addEventListener('click', startTimer));

e9ef7a437c38079d23b0978ee508fc9bfd7254ce
Edwin Chen <edwinchen85@gmail.com>
2017-05-26 17:42:49 +0800

    Format minute to have leading zero

--- scripts.js
+++ scripts.js
@@ -32,5 +32,5 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
-  endTime.textContent = `Be Back At ${hour > 12 ? hour -12 : hour}:${minutes}`;
+  endTime.textContent = `Be Back At ${hour > 12 ? hour -12 : hour}:${minutes < 10 ? '0' : ''}${minutes}`;
 }

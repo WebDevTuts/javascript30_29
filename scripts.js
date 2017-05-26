a0bfe5dc4965b42afaf5c4f3086fafb3b0f53faf
Edwin Chen <edwinchen85@gmail.com>
2017-05-26 17:38:44 +0800

    Display end time

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -30,4 +32,5 @@ function displayEndTime(timestamp) {
   const end = new Date(timestamp);
   const hour = end.getHours();
   const minutes = end.getMinutes();
+  endTime.textContent = `Be Back At ${hour}:${minutes}`;
 }
